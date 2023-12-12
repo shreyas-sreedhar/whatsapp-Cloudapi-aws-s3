0ab241184e8c5ca4c34d3caff1a277b018f0df38
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2023-12-12 12:27:32 -0500

    Added aws connection file

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 console.log("Hello from Indexjs");
 
+
 const express = require("express");
 const body_parser = require("body-parser");
 const axios = require("axios");
 const fs = require("fs");
+const s3 = require("./s3"); //Adding the aws s3 connection file.
 
 require('dotenv').config();
-const s3 = require("./s3");
+
 const sharp = require('sharp');
-const stream = require('stream');
 
 const app = express().use(body_parser.json());
 
@@ -17,6 +18,9 @@ const token = process.env.TOKEN;
 const mytoken = process.env.MYTOKEN;
 const yourAuthToken = token;
 
+
+//Remove "8080 ||" if you're hosting it in heroku or elsewhere
+
 app.listen(8080 || process.env.PORT, () => {
-    console.log("webhook is listening");
+    console.log("Website is Running");
 });
