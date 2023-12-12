88cc088e08a43785ceac2663a29726b29ab3a4c7
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2023-12-12 06:03:34 -0500

    Added Env and import statements

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("Hello from Indexjs");
+
+const express = require("express");
+const body_parser = require("body-parser");
+const axios = require("axios");
+const fs = require("fs");
+
+require('dotenv').config();
+const s3 = require("./s3");
+const sharp = require('sharp');
+const stream = require('stream');
+
+const app = express().use(body_parser.json());
+
+
+const token = process.env.TOKEN;
+const mytoken = process.env.MYTOKEN;
+const yourAuthToken = token;
+
+app.listen(8080 || process.env.PORT, () => {
+    console.log("webhook is listening");
+});
